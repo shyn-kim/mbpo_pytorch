645b5ea5a0c0bac24bbb14bb96e1d7b83423df87
Suhyeon Kim <ksh0104@snu.ac.kr>
2025-09-23 13:49:31 +0900

    match i/o with updated api

--- predict_env.py
+++ predict_env.py
@@ -13,10 +13,12 @@ class PredictEnv:
 
             height = next_obs[:, 0]
             angle = next_obs[:, 1]
-            not_done = np.isfinite(next_obs).all(axis=-1) \
-                       * np.abs(next_obs[:, 1:] < 100).all(axis=-1) \
-                       * (height > .7) \
-                       * (np.abs(angle) < .2)
+            not_done = (
+                np.isfinite(next_obs).all(axis=-1)
+                * np.abs(next_obs[:, 1:] < 100).all(axis=-1)
+                * (height > 0.7)
+                * (np.abs(angle) < 0.2)
+            )
 
             done = ~not_done
             done = done[:, None]
@@ -26,42 +28,47 @@ class PredictEnv:
 
             height = next_obs[:, 0]
             angle = next_obs[:, 1]
-            not_done = (height > 0.8) \
-                       * (height < 2.0) \
-                       * (angle > -1.0) \
-                       * (angle < 1.0)
+            not_done = (height > 0.8) * (height < 2.0) * (angle > -1.0) * (angle < 1.0)
             done = ~not_done
             done = done[:, None]
             return done
-        elif 'walker_' in env_name:
-            torso_height =  next_obs[:, -2]
+        elif "walker_" in env_name:
+            torso_height = next_obs[:, -2]
             torso_ang = next_obs[:, -1]
-            if 'walker_7' in env_name or 'walker_5' in env_name:
-                offset = 0.
+            if "walker_7" in env_name or "walker_5" in env_name:
+                offset = 0.0
             else:
                 offset = 0.26
-            not_done = (torso_height > 0.8 - offset) \
-                       * (torso_height < 2.0 - offset) \
-                       * (torso_ang > -1.0) \
-                       * (torso_ang < 1.0)
+            not_done = (
+                (torso_height > 0.8 - offset)
+                * (torso_height < 2.0 - offset)
+                * (torso_ang > -1.0)
+                * (torso_ang < 1.0)
+            )
             done = ~not_done
             done = done[:, None]
             return done
-        elif 'HalfCheetah' in env_name:
+        elif "HalfCheetah" in env_name:
             batch_size = obs.shape[0]
-            return np.zeros(shape=(batch_size, 1), dtype=np.bool) # HalfCheetah never terminates
-        elif 'Ant' in env_name: # 0916 KSH
-            healthy_z_min, healthy_z_max =(0.2, 1.0)
+            return np.zeros(
+                shape=(batch_size, 1), dtype=np.bool
+            )  # HalfCheetah never terminates
+        elif "Ant" in env_name:  # 0916 KSH
+            healthy_z_min, healthy_z_max = (0.2, 1.0)
             if next_obs.shape[-1] == 105:
-                next_z = next_obs[:,0]
+                next_z = next_obs[:, 0]
             else:
-                next_z = next_obs[:,2]
+                next_z = next_obs[:, 2]
 
-            not_done = (next_z <= healthy_z_max) * (next_z >= healthy_z_min) * np.isfinite(next_obs).all(axis=-1)
-            done = ~ not_done
+            not_done = (
+                (next_z <= healthy_z_max)
+                * (next_z >= healthy_z_min)
+                * np.isfinite(next_obs).all(axis=-1)
+            )
+            done = ~not_done
             done = done[:, None]
             return done
-        else: # default (never terminates)
+        else:  # default (never terminates)
             batch_size = obs.shape[0]
             return np.zeros(shape=(batch_size, 1), dtype=np.bool)
 
@@ -70,7 +77,15 @@ class PredictEnv:
         k = x.shape[-1]
 
         ## [ num_networks, batch_size ]
-        log_prob = -1 / 2 * (k * np.log(2 * np.pi) + np.log(variances).sum(-1) + (np.power(x - means, 2) / variances).sum(-1))
+        log_prob = (
+            -1
+            / 2
+            * (
+                k * np.log(2 * np.pi)
+                + np.log(variances).sum(-1)
+                + (np.power(x - means, 2) / variances).sum(-1)
+            )
+        )
 
         ## [ batch_size ]
         prob = np.exp(log_prob).sum(0)
@@ -92,8 +107,12 @@ class PredictEnv:
 
         inputs = np.concatenate((obs, act), axis=-1)
 
-        if 'LNN' in self.model_type: # (0828 KSH - LNN based transition model) (0911 KSH - modified to accept 'LNN' and 'LNN_ensemble')
-            rewards, next_obs = self.model.predict(inputs, factored=True) # return as numpy
+        if (
+            "LNN" in self.model_type
+        ):  # (0828 KSH - LNN based transition model) (0911 KSH - modified to accept 'LNN' and 'LNN_ensemble')
+            rewards, next_obs, _, _ = self.model.predict(
+                inputs, factored=True
+            )  # return as numpy
             terminals = self._termination_fn(self.env_name, obs, act, next_obs)
 
             if return_single:
@@ -104,21 +123,29 @@ class PredictEnv:
             info = {}
 
         else:
-            if self.model_type == 'pytorch':
-                ensemble_model_means, ensemble_model_vars = self.model.predict(inputs)            
+            if self.model_type == "pytorch":
+                ensemble_model_means, ensemble_model_vars = self.model.predict(inputs)
             else:
-                ensemble_model_means, ensemble_model_vars = self.model.predict(inputs, factored=True)
+                ensemble_model_means, ensemble_model_vars = self.model.predict(
+                    inputs, factored=True
+                )
             ensemble_model_means[:, :, 1:] += obs
             ensemble_model_stds = np.sqrt(ensemble_model_vars)
 
             if deterministic:
                 ensemble_samples = ensemble_model_means
             else:
-                ensemble_samples = ensemble_model_means + np.random.normal(size=ensemble_model_means.shape) * ensemble_model_stds
+                ensemble_samples = (
+                    ensemble_model_means
+                    + np.random.normal(size=ensemble_model_means.shape)
+                    * ensemble_model_stds
+                )
 
             num_models, batch_size, _ = ensemble_model_means.shape
-            if self.model_type == 'pytorch':
-                model_idxes = np.random.choice(self.model.elite_model_idxes, size=batch_size)
+            if self.model_type == "pytorch":
+                model_idxes = np.random.choice(
+                    self.model.elite_model_idxes, size=batch_size
+                )
             else:
                 model_idxes = self.model.random_inds(batch_size)
             batch_idxes = np.arange(0, batch_size)
@@ -127,14 +154,21 @@ class PredictEnv:
             model_means = ensemble_model_means[model_idxes, batch_idxes]
             model_stds = ensemble_model_stds[model_idxes, batch_idxes]
 
-            log_prob, dev = self._get_logprob(samples, ensemble_model_means, ensemble_model_vars)
+            log_prob, dev = self._get_logprob(
+                samples, ensemble_model_means, ensemble_model_vars
+            )
 
             rewards, next_obs = samples[:, :1], samples[:, 1:]
             terminals = self._termination_fn(self.env_name, obs, act, next_obs)
 
             batch_size = model_means.shape[0]
-            return_means = np.concatenate((model_means[:, :1], terminals, model_means[:, 1:]), axis=-1)
-            return_stds = np.concatenate((model_stds[:, :1], np.zeros((batch_size, 1)), model_stds[:, 1:]), axis=-1)
+            return_means = np.concatenate(
+                (model_means[:, :1], terminals, model_means[:, 1:]), axis=-1
+            )
+            return_stds = np.concatenate(
+                (model_stds[:, :1], np.zeros((batch_size, 1)), model_stds[:, 1:]),
+                axis=-1,
+            )
 
             if return_single:
                 next_obs = next_obs[0]
@@ -143,6 +177,11 @@ class PredictEnv:
                 rewards = rewards[0]
                 terminals = terminals[0]
 
-            info = {'mean': return_means, 'std': return_stds, 'log_prob': log_prob, 'dev': dev}
+            info = {
+                "mean": return_means,
+                "std": return_stds,
+                "log_prob": log_prob,
+                "dev": dev,
+            }
 
         return next_obs, rewards, terminals, info
